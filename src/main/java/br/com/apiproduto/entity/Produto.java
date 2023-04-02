23cc3b7fea140093b75d9d4d8aa4137114e17c86
Fábio Souza <fhssouza@gmail.com>
2023-04-01 21:22:53 -0300

    start do projeto

---
+++ src/main/java/br/com/apiproduto/entity/Produto.java
@@ -0,0 +1,71 @@
+package br.com.apiproduto.entity;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
+	private String nome;
+	
+	@Column
+	private BigDecimal valor;
+	
+	@Column
+	private String descricao;
+	
+	public Produto() {
+	}
+
+	public Produto(Long id, String nome, BigDecimal valor, String descricao) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.valor = valor;
+		this.descricao = descricao;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
